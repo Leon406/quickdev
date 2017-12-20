91233057f241104602d5e3b7a99dc7445576d348
Leon <deadogone@gmail.com>
2017-12-20 23:59:01 +0800

    统一管理依赖

---
+++ app/src/main/java/me/leon/quickdev/App.java
@@ -0,0 +1,18 @@
+package me.leon.quickdev;
+
+import android.app.Application;
+
+/**
+ * Author : Leon
+ * E-mail : deadogone@gmail.com
+ * Time   : 2017/12/20 0020 23:57
+ * Desc   : This is App
+ * Version: 1.0.1
+ */
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
